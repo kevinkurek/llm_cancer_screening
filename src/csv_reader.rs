12354b5cdb8479dfedfd487fad71672ced81212b
kevinkurek <kevinkurek@live.com>
2024-12-28 16:39:08 -0800

    add error handling

--- src/csv_reader.rs
+++ src/csv_reader.rs
@@ -1,33 +1,24 @@
-// use polars::prelude::CsvReadOptions;
-// use polars_core::prelude::*;
-// use polars::prelude::*;
 use polars::prelude::*;
 use std::fs::File;
 
 pub fn read_csv(file_path: &str) -> PolarsResult<DataFrame> {
     let df = CsvReadOptions::default()
             .with_has_header(true)
-            .try_into_reader_with_file_path(Some(file_path.into()))
-            .unwrap()
-            .finish()
-            .unwrap();
-    println!("{:?}", df);
+            .try_into_reader_with_file_path(Some(file_path.into()))?
+            .finish()?;
     Ok(df)
 }
 
 pub fn extract_text_inputs(df: &DataFrame) -> PolarsResult<Vec<String>> {
 
     // get the series of the column "Text_Input"
-    let text_input_col = df.column("Text_Input")?;
+    let text_input_col = df.column("Text_Input")?.str()?;
 
     // Convert to a Vec<String>
     let text_inputs: Vec<String> = text_input_col
-            .str() // Access the Series as a String type
-            .expect("Column is not a String type") // Ensure the column is of type String
             .into_iter() // Get an iterator over the column values
-            .map(|opt_name| opt_name.unwrap_or("").to_string()) // Handle Option<&str> and convert to String
+            .map(|val| val.unwrap_or("").to_string()) // Convert the Option<&str> to a String
             .collect(); // Collect the iterator into a Vec<String>
-    // println!("{:?}", text_inputs);
 
     Ok(text_inputs)
 }
--- src/main.rs
+++ src/main.rs
@@ -28,10 +28,25 @@ async fn main() {
         (api_url, api_key)
     };
 
-    // Use a relative path for the CSV file
-    let mut df = read_csv(FILE_PATH).expect("Failed to read CSV");
-    let text_inputs = extract_text_inputs(&df).expect("Failed to extract Text_Input column");
-    println!("Text inputs: {:?}", text_inputs);
+    // Handle reading the CSV file
+    let mut df = match read_csv(FILE_PATH) {
+        Ok(df) => df,
+        Err(e) => {
+            eprintln!("Error reading CSV into dataframe: {}", e);
+            return;
+        }
+    };
+    println!("First 5 dataframe values: {}", df.head(Some(5)));
+
+    // Extract the text inputs from the dataframe
+    let text_inputs = match extract_text_inputs(&df) {
+        Ok(text_inputs) => text_inputs,
+        Err(e) => {
+            eprintln!("Error extracting text inputs from dataframe: {}", e);
+            return;
+        }
+    };
+    println!("First 5 Example Text inputs: {:?}", &text_inputs[..5]);
 
     // Create a vector of futures for the API calls: wrap api_url and api_key in Arc to share across threads
     let api_url = Arc::new(api_url);
@@ -42,5 +57,8 @@ async fn main() {
     let results: Vec<Option<String>> = join_all(futures).await.into_iter().map(|res| res.unwrap()).collect();
 
     // Add the results to the new column and write to the output CSV
-    add_column_and_write_csv(&mut df, "Cancer_Detected", results, OUTPUT_PATH).expect("Failed to write CSV");
+    if let Err(e) = add_column_and_write_csv(&mut df, "Cancer_Detected", results, OUTPUT_PATH){
+        eprintln!("Error adding column and writing to CSV: {}", e);
+        return;
+    };
 }
